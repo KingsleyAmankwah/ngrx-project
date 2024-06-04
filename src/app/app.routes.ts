d5c1727a2f2fd67cbe7bd31d837e9492bf321bbb
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-06-04 16:47:11 +0000

    app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/auth/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+
+  {
+    path: '',
+    loadChildren: () =>
+      import('./pages/dashboard/dashboard-routing.module').then(
+        (m) => m.dashboardRoutes
+      ),
+  },
+
+  {
+    path: '**',
+    redirectTo: 'login',
+  },
+
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+];
